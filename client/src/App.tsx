13ca7667bb1e139c9ea34ae3c38f65a55a8d1e9e
keyurkakadiya53 <kakadiyakeyur53@gmail.com>
2025-09-24 20:19:37 +1000

    uodate file to load dashboard at all

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,12 @@ import { Switch, Route } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Dashboard from "@/pages/dashboard";
-import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Dashboard} />
-      <Route component={NotFound} />
+      <Route component={Dashboard} />
     </Switch>
   );
 }
